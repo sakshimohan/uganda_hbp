acc1aa574dd2988c0ab4effcfe7ebfbfa5d3e1a0
sm2511 <sakshi.mohan08@gmail.com>
2022-12-07 19:40:17 +0000

    remove OHT maximum feasible coverage constraints

--- 1_script/3_outputs_for_healthecon_manuscript.R
+++ 1_script/3_outputs_for_healthecon_manuscript.R
@@ -3,6 +3,23 @@
 # Extract resource use and case numbers for scenario 7 and 8
 # solution_hruse, solution_drugexp
 
+# Old assumptions
+#comp_nested_list2 = NULL
+#comp_nested_list2 = NULL
+#subs_list = list(subs1 = c("104", "105", "106"), # First line treatment of smear positive cases, Full DOTS (smear-positive, smear negative and Extrapulmonary cases), Full combination DOTS  (smear-positive, smear negative, extrapulmonary cases, MDR cases)
+#                 subs2 = c("112","113"), # Viral load testing, CD4 testing
+#                 subs3 = c("187", "188"), # Treatment of injuries (Fracture and dislocation - reduction), Treatment of injuries (Fracture and dislocation - fixation)
+#                 subs4 = c("217", "218"), # Prevention of cardiovascular disease, Prevention and treatment of cardiovascular disease
+#                 subs5 = c("316", "317"), # Xpert test (Full), Xpert test (targeted)**
+#                 subs6 = c("185", "186"), # Colorectoral cancer (screening + treatment), Colorectoral cancer (treatment)**
+#                 subs7 = c("321", "322")) # ART (Second-Line Treatment) for adults without intensive monitoring, ART (Second-Line Treatment) for adults with intensive monitoring
+
+feascov_assumption = 0
+feascov_scale_assumption = 1
+base.cet = 161
+drugbg_assumption = 1
+base.hr = rep(1,8)
+
 # Scenarios
 ##########################################################
 scenarios = c("base", "taskshifting") # for file names
@@ -10,20 +27,20 @@ scenario_labels = c("Base scenario", "Task-shifting scenario") # for table heade
 
 # Base scenario
 find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                                       drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                                       hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                                       drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                                       hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                                        compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                                        complements_nested2 = comp_nested_list2, task_shifting_pharm = 0)
-summary_base = cbind.data.frame(intervention.count, dalys_averted, solution.class$objval, cet_soln, drug_exp.prop, t(hruse.prop[,visible_cadres]))
+summary_base = cbind.data.frame(pos_nethealth.count, intervention.count, dalys_averted, solution.class$objval, cet_soln, drug_exp.prop, t(hruse.prop[,visible_cadres]))
 optimal_coverage_base = solution
 
 # Task-shifting scenario
 task_shifting_scenario  <- find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                                                drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                                                hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                                                drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                                                hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                                                 compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                                                 complements_nested2 = comp_nested_list2, task_shifting_pharm = 1)
-summary_taskshifting = cbind.data.frame(intervention.count, dalys_averted, solution.class$objval, cet_soln, drug_exp.prop, t(hruse.prop[,visible_cadres]))
+summary_taskshifting = cbind.data.frame(pos_nethealth.count, intervention.count, dalys_averted, solution.class$objval, cet_soln, drug_exp.prop, t(hruse.prop[,visible_cadres]))
 optimal_coverage_taskshifting = solution
 
 # STable 1: Date Summary
@@ -33,7 +50,7 @@ optimal_coverage_taskshifting = solution
 ##########################################################
 summary = rbind(summary_base, summary_taskshifting)
 summary = cbind(scenario_labels, summary)
-colnames(summary) = c("Scenario", "Number of interventions in the optimal package", 
+colnames(summary) = c("Scenario", "Number of interventions with a positive Net Health Benefit", "Number of interventions in the optimal package", 
                       "Total DALYs averted","Net DALYs averted", "Highest ICER in the optimal pacakge", "Percentage of drug budget required",
                       "Percentage of Doctor/Clinical officer capacity required", "Percentage of Nursing staff capacity required",
                       "Percentage of Pharmaceutical staff capacity required", 
@@ -47,8 +64,8 @@ write.csv(t(summary), file = "4_outputs/tables/table_2_result_summary.csv")
 # Base
 capture.output(
   find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                       drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                       hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                       drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                       hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                        compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                        complements_nested2 = comp_nested_list2, task_shifting_pharm = 0)
 )
@@ -63,8 +80,8 @@ program_summary_pt1[,4] <- program_summary_pt1[,3]/program_summary_pt1[,2]
 # Task_shifting
 capture.output(
   find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                       drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                       hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                       drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                       hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                        compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                        complements_nested2 = comp_nested_list2, task_shifting_pharm = 1)
 )
@@ -90,8 +107,8 @@ write.csv(program_summary, file = "4_outputs/tables/table_3_program_summary.csv"
 # Results on chosen package/coverage under various scenarios
 for (task_shifting_scen in c(1,2)){
   find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                       drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                       hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                       drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                       hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                        compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                        complements_nested2 = comp_nested_list2, task_shifting_pharm = task_shifting_scen-1)
   
@@ -150,8 +167,8 @@ write.csv(t(summary_4scens), file = "4_outputs/tables/stable_6_result_summary.cs
 ##########################################################
 for (task_shifting_scen in c(1,2)){
   find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                       drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                       hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                       drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                       hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                        compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                        complements_nested2 = comp_nested_list2, task_shifting_pharm = task_shifting_scen-1)
   gen_resourceuse_graphs("", paste0("4_outputs/figures/resourceuse_", scenarios[task_shifting_scen], ".png"))
@@ -171,8 +188,8 @@ for (task_shifting_scen in c(1,2)){
   
   capture.output(
     find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                         drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                         hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                         drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                         hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                          compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                          complements_nested2 = comp_nested_list2, task_shifting_pharm = task_shifting_scen - 1)
   
@@ -183,8 +200,8 @@ for (task_shifting_scen in c(1,2)){
   # Marginal value of consumables
   capture.output(
     find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                         drug_budget_input = base.drugbudget+1, drug_budget.scale = 1, 
-                         hr.scale = base.hr, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                         drug_budget_input = base.drugbudget+1, drug_budget.scale = drugbg_assumption, 
+                         hr.scale = base.hr, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                          compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                          complements_nested2 = comp_nested_list2, task_shifting_pharm = task_shifting_scen - 1)
     
@@ -199,15 +216,15 @@ for (task_shifting_scen in c(1,2)){
     print(cadre_names[cadre])
     #noquote(paste0("prop_1",  cadre_names[cadre]))
     
-    prop <- 1/cons_hr.limit.saved[,cadre]
+    prop <- 1/cons_hr.limit.saved[,cadre] # Note that this already takes into account scaling
 
-    hr.scale.marginal <- rep(1,8)
+    hr.scale.marginal <- base.hr
     hr.scale.marginal[cadre] <- hr.scale.marginal[cadre] + prop
     
     capture.output(
       find_optimal_package(data.frame = chosen_df, objective_input = 'nethealth', cet_input = base.cet, 
-                           drug_budget_input = base.drugbudget, drug_budget.scale = 1, 
-                           hr.scale = hr.scale.marginal, use_feasiblecov_constraint = 1, feascov_scale = 1, compcov_scale = 1,
+                           drug_budget_input = base.drugbudget, drug_budget.scale = drugbg_assumption, 
+                           hr.scale = hr.scale.marginal, use_feasiblecov_constraint = feascov_assumption, feascov_scale = feascov_scale_assumption, compcov_scale = 1,
                            compulsory_interventions = NULL, substitutes = subs_list, complements_nested1 = comp_nested_list1,
                            complements_nested2 = comp_nested_list2, task_shifting_pharm = task_shifting_scen - 1)
 
@@ -229,6 +246,13 @@ resource_labels <- c(cadre_labels[visible_cadres], "Consumables budget")
 
 
 for (task_shifting_scen in c(1,2)){
+  if (task_shifting_scen == 1){
+    xlim_scen = 3000
+    label_placement = 240
+  } else{
+    xlim_scen = 30
+    label_placement = 2
+  }
   
   margval <- margval_1000_table[task_shifting_scen,]
   filename <- paste0("4_outputs/figures/margval_", scenarios[task_shifting_scen], ".pdf")
@@ -237,10 +261,10 @@ for (task_shifting_scen in c(1,2)){
   
   y <- barplot(height = rev(t(margval)), names = rev(resource_labels), width = 1 ,
                space=NULL, las = 1, horiz=T,
-               xlim=c(0,3000), ylab = "", xlab = "Net DALYs averted",
+               xlim=c(0,xlim_scen), ylab = "", xlab = "Net DALYs averted",
                main="")
   x <-as.matrix(rev(t(margval)))
-  text(x+200,y,labels=as.character(round(x,3)))
+  text(x+label_placement,y,labels=as.character(round(x,3)))
   dev.off()
 }
 
